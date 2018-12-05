ad21d0fc25bf39187b9a7e459d3de3651768bf9c
Makentoshe <mkliverout@gmail.com>
2018-12-06 02:55:37 +0300

    [feature] Update ViewHolder

--- app/src/main/java/health/officetv/jetbrains/org/officetvhealthchecker/main/view/ViewHolder.kt
+++ app/src/main/java/health/officetv/jetbrains/org/officetvhealthchecker/main/view/ViewHolder.kt
@@ -1,22 +1,30 @@
 package health.officetv.jetbrains.org.officetvhealthchecker.main.view
 
 import android.content.Context
-import org.jetbrains.anko.dip
-import org.jetbrains.anko.matchParent
-import org.jetbrains.anko.textView
-import org.jetbrains.anko.verticalLayout
+import android.graphics.Color
 import health.officetv.jetbrains.org.officetvhealthchecker.R
+import org.jetbrains.anko.*
 
 class ViewHolder(context: Context) {
 
     @JvmField
-    val view = context.verticalLayout {
+    val view = context.relativeLayout {
+        setPadding(dip(8), dip(4), dip(8), dip(4))
+        lparams(width = matchParent)
         textView {
             id = R.id.text_name
-        }.lparams(width = matchParent, height = dip(20))
+            textSize = 16f
+            textColor = Color.BLACK
+        }.lparams(width = matchParent, height = dip(30)) {
+            alignParentTop()
+        }
 
         textView {
             id = R.id.text_url
-        }.lparams(width = matchParent, height = dip(20))
+            setPadding(dip(8), 0, 0, 0)
+            singleLine = true
+        }.lparams(width = matchParent, height = dip(20)) {
+            below(R.id.text_name)
+        }
     }
 }
