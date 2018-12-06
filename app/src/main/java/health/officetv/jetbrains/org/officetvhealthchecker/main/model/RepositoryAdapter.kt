1198451733209f6b243f3cf556ef60975a8dd25c
Makentoshe <mkliverout@gmail.com>
2018-12-06 18:38:34 +0300

    [refactor] Remake RepositoryAdapter element view building

--- app/src/main/java/health/officetv/jetbrains/org/officetvhealthchecker/main/model/RepositoryAdapter.kt
+++ app/src/main/java/health/officetv/jetbrains/org/officetvhealthchecker/main/model/RepositoryAdapter.kt
@@ -1,5 +1,6 @@
 package health.officetv.jetbrains.org.officetvhealthchecker.main.model
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
@@ -10,12 +11,12 @@ import health.officetv.jetbrains.org.officetvhealthchecker.main.view.ViewHolder
 
 class RepositoryAdapter(private val repository: ApiRepository) : BaseAdapter() {
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View = with(parent.context) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View  {
         val data = getItem(position)
-        val view = convertView ?: ViewHolder(this, this@RepositoryAdapter, repository).view
-        view.findViewById<TextView>(R.id.text_name).text = data.name
-        view.findViewById<TextView>(R.id.text_url).text = data.url
-        return@with view
+        return buildView(convertView, parent.context).apply {
+            findViewById<TextView>(R.id.text_name).text = data.name
+            findViewById<TextView>(R.id.text_url).text = data.url
+        }
     }
 
     override fun getItem(position: Int) = repository.getAll()[position]
@@ -23,5 +24,9 @@ class RepositoryAdapter(private val repository: ApiRepository) : BaseAdapter() {
     override fun getItemId(position: Int) = position.toLong()
 
     override fun getCount() = repository.size()
+
+    private fun buildView(convertView: View?, context: Context): View {
+        return convertView ?: ViewHolder(context, this@RepositoryAdapter, repository).view
+    }
 
 }
